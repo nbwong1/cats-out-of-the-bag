48a0c07797e1b9e8625162f1d78c5b7ee74c31eb
nbwong <nicholas.b.wong@gmail.com>
2022-08-01 20:05:30 -0700

    Completed local storage code to save as array.

--- js/catFacts.js
+++ js/catFacts.js
@@ -1,6 +1,10 @@
 var factRequestUrl = "https://catfact.ninja/fact";
 var catFact = $("random-facts");
-//var obj = JSON.parse(response);
+var catFactList = $("#cat-fact-list");
+var catFactSpan = $("#cat-fact-span");
+
+// global variable where we will store all response.json
+const catFacts = [];
 
 console.log(factRequestUrl);
 meowButton = $("#Meow-button");
@@ -15,15 +19,52 @@ function handleButtonClick() {
     .then(function (data) {
       console.log(data);
       $("#random-facts").text(data.fact);
-      // saves the data.fact within local storage; todo: but need to save it and create list.
-      var catFactStorage = JSON.stringify(data.fact);
-      localStorage.setItem("catfacts", catFactStorage);
-    })
-   
 
-  }
+      var catFactArray = JSON.parse(localStorage.getItem('catfacts')) || [];
+      
+      catFactArray.push(data.fact);
+      console.log(catFactArray);
+      localStorage.setItem("catfacts", JSON.stringify(catFactArray));
+
+      // saves the data.fact within local storage; todo: but need to save it and create list.
+      // var catFactStorage = JSON.stringify(data.fact);
+      // localStorage.setItem("catfacts", catFactStorage);
+
+      // let newCatFactStorage = catFactStorage[i];
+
+      // localStorage.setItem("catfacts", JSON.stringify(newCatFact));
+    });
+  // begin code
+  // function renderCatFacts() {
+  //   catFactList.innerHTML = "";
+  //   catFactSpan.textContent = catFacts.length;
+
+  //   for (var i = 0; i < catFacts.length; i++) {
+  //     var catFact = catFacts[i];
+  //   }
+  // }
+
+  // function init() {
+  //   var storedCatFacts = JSON.parse(localStorage.getItem("catfacts"));
+
+  //   if (storedCatFacts !== null) {
+  //     catFacts = storedCatFacts;
+  //   }
+  //   renderCatFacts();
+  // }
+
+  // function storeCatFacts() {
+  //   localStorage.setItem("catfacts", JSON.stringify(catFacts));
+
+  //   storeCatFacts();
+  //   renderCatFacts();
+  // }
+  // end code
+}
 
 // #catFactHistory
 console.log(catFact);
 
 // save #random-facts in local storage
+
+// blocked out code
