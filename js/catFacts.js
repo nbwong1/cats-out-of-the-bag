19c9033e8e760f6243e3eacb2f193ac21917080b
nbwong <nicholas.b.wong@gmail.com>
2022-08-01 21:33:10 -0700

    added CSS and made the document responsive. Included materialize format

--- js/catFacts.js
+++ js/catFacts.js
@@ -19,52 +19,15 @@ function handleButtonClick() {
     .then(function (data) {
       console.log(data);
       $("#random-facts").text(data.fact);
-
+// check if this is an array, if data is already saved to local storage
       var catFactArray = JSON.parse(localStorage.getItem('catfacts')) || [];
-      
+      // push new data to local storage
       catFactArray.push(data.fact);
       console.log(catFactArray);
       localStorage.setItem("catfacts", JSON.stringify(catFactArray));
 
-      // saves the data.fact within local storage; todo: but need to save it and create list.
-      // var catFactStorage = JSON.stringify(data.fact);
-      // localStorage.setItem("catfacts", catFactStorage);
-
-      // let newCatFactStorage = catFactStorage[i];
-
-      // localStorage.setItem("catfacts", JSON.stringify(newCatFact));
     });
-  // begin code
-  // function renderCatFacts() {
-  //   catFactList.innerHTML = "";
-  //   catFactSpan.textContent = catFacts.length;
-
-  //   for (var i = 0; i < catFacts.length; i++) {
-  //     var catFact = catFacts[i];
-  //   }
-  // }
-
-  // function init() {
-  //   var storedCatFacts = JSON.parse(localStorage.getItem("catfacts"));
-
-  //   if (storedCatFacts !== null) {
-  //     catFacts = storedCatFacts;
-  //   }
-  //   renderCatFacts();
-  // }
-
-  // function storeCatFacts() {
-  //   localStorage.setItem("catfacts", JSON.stringify(catFacts));
-
-  //   storeCatFacts();
-  //   renderCatFacts();
-  // }
-  // end code
 }
 
 // #catFactHistory
 console.log(catFact);
-
-// save #random-facts in local storage
-
-// blocked out code
